254403ed2eecabf4ca589520728bbefd7e93a52f
joshuakrempasky <joshkrempasky@gmail.com>
2022-09-18 23:20:30 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int num1 = 2;
+        int num2 = 18;
+        int num3 = 7;
+
+        if (num1 % num1 == 0)
+            System.out.println(num1 + " is even.");
+        else
+            System.out.println(num1 + " is odd.");
+
+        if (num2 % num2 == 0)
+            System.out.println(num2 + " is even.");
+        else
+            System.out.println(num2 + " is odd.");
+
+        if (num3 % num3 == 0)
+            System.out.println(num3 + " is even.");
+        else
+            System.out.println(num3 + " is odd.");
+    }
+}
